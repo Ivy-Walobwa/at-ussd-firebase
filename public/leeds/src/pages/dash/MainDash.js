ac827aef78249dab2f7c4b9c0b037466805a8151
Muremwa <danmburu254@gmail.com>
2021-03-29 15:45:19 +0300

    names section complete

--- public/leeds/src/pages/dash/MainDash.js
+++ public/leeds/src/pages/dash/MainDash.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { Container, Row  } from 'react-bootstrap';
 
 import FilterSection from './Filters';
+import NamesSection from './Names';
 
 import '../../css/dash.css';
 
@@ -12,6 +13,7 @@ export default function Dash () {
         <Container fluid>
             <Row>
                 <FilterSection />
+                <NamesSection />
             </Row>
         </Container>
     )
